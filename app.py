9ebae3c1d9c55f51423115bad6d1b377e181f90b
Gaurab <gaurabbhattarai29@gmail.com>
2023-11-03 15:11:12 +1030

    V3 Working mediapipe landmark drawing, needs some work to be more accurate

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import os
 from flask import Flask, render_template, url_for, request
 from werkzeug.utils import secure_filename
 from datetime import datetime
+import numpy as np
+import mediapipe as mp
+from matplotlib import pyplot as plt
 import cv2
 
-
 app = Flask(__name__)
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
 
 
 @app.route('/')
@@ -51,6 +55,41 @@ def video():
     return render_template('upload_panel.html', video_files=video_files)
 
 
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+def mediapipe_detection(image, holistic):
+    # Perform the detection using the holistic object
+    results = holistic.process(image)
+    return image, results
+
+
 @app.route('/upload-panel/<video_filename>')
 def video_panel(video_filename):
     selected_video_filename = video_filename
@@ -63,9 +102,6 @@ def video_panel(video_filename):
 
     cap = cv2.VideoCapture(selected_video_path)
 
-    if not cap.isOpened():
-        return "Unable to read video"
-
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
 
@@ -73,28 +109,34 @@ def video_panel(video_filename):
     output_video_path = os.path.join(video_directory, output_filename)
     processed_video_url = url_for('static', filename='videos/' + output_filename)
 
-    print('new_video_path = ' + output_video_path)
-    print('selected_video_path = ' + selected_video_path)
-    print('output_video_path = ' + output_video_path)
-
     fourcc = cv2.VideoWriter_fourcc(*'H264')
     fps = 15
 
     out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
+    frames = []
 
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            out.write(frame)
-        else:
-            break
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame_array = np.asarray(frame)
+            image, results = mediapipe_detection(frame, holistic)
+            draw_styled_landmarks(image, results)
+
+            out.write(image)  # Write the processed frame with landmarks to the output video
+
+            frames.append(frame_array)
 
     cap.release()
     out.release()
 
     if os.path.exists(output_video_path):
         print('Video found')
-        return render_template('upload_panel_videos.html', selected_video_url=selected_video_url, processed_video_url=processed_video_url)
+        return render_template('upload_panel_videos.html', selected_video_url=selected_video_url,
+                               processed_video_url=processed_video_url)
+
 
     else:
         print('Video not found')
